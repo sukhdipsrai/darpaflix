9bcd1e0a304b8e0cb11f777fd70150683b2ddd15
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-01-19 16:00:56 -0500

    static page setup

---
+++ frontend/root.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+class Root extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <h2>
+                This is the Darpaflix App.
+            </h2>
+        )
+    }
+}
+
+export default Root;
