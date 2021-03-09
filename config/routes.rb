e133cc2020599c1a2b3f0ea04f268d1ea7e0cf97
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-03-08 22:18:48 -0500

    aws config

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json } do 
     resources :users, only: [:create]
     resources :session, only: [:create, :destroy]
-    resources :videos, only:[:get]
+    resources :videos, only:[:index]
   end
   
 end
