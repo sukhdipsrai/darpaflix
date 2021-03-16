40a7444695b26c9f4dbf9a81a8609da1ba0f3d07
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-03-16 01:06:31 -0400

    movie migration

--- db/migrate/20210316034813_create_movies.rb
+++ db/migrate/20210316034813_create_movies.rb
@@ -1,8 +1,14 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-
+      t.string :title,null: false
+      t.string :cast,null: false
+      t.string :director,null: false
+      t.text :summary,null: false
+      t.integer :year, null: false
+      t.float :duration,null: false
       t.timestamps
     end
+
   end
 end
