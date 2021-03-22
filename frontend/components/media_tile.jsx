34a7d3652159d0e196efee977e59e45376391cfe
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-03-22 02:51:24 -0400

    super modal created

--- frontend/components/media_tile.jsx
+++ frontend/components/media_tile.jsx
@@ -5,6 +5,7 @@ class MediaTile extends React.Component {
     super(props);
     this.state = {
       trailerMode: false,
+      superModal: false,
     };
     this.timer = 0;
     this.mouseHover = false;
@@ -13,7 +14,7 @@ class MediaTile extends React.Component {
   hoverTimer() {
     if (this.timer >= 600) {
       document.getElementById(this.props.id).classList.add(this.modalClass);
-      this.setState({ trailerMode: true });
+      this.setState({ trailerMode: true, superModal: false });
       this.playVideoListner();
     } else if (this.mouseHover)
       setTimeout(() => {
@@ -37,7 +38,7 @@ class MediaTile extends React.Component {
     document.getElementById(this.props.id).classList.remove(this.modalClass);
     this.timer = 0;
     this.mouseHover = false;
-    this.setState({ trailerMode: false });
+    this.setState({ trailerMode: false, superModal: true });
   }
   render() {
     this.modalClass = "modal";
@@ -45,7 +46,11 @@ class MediaTile extends React.Component {
     else if (this.props.last) this.modalClass = "modal-last";
     // let displayContent = <img src={this.props.data.imageUrl}></img>;
     let displayContent = <img src={window.devImageUrl}></img>;
+    let superModal = null;
     let buttonContent = null;
+    let videoContent = null;
+    let modalBack = null;
+    let entireModal = null;
     if (this.state.trailerMode) {
       displayContent = (
         <video
@@ -75,6 +80,33 @@ class MediaTile extends React.Component {
         </div>
       );
     }
+    if (this.state.superModal) {
+      superModal = (
+        <video
+          className="super-media-video"
+          // src={this.props.data.trailerUrl}
+          src={window.devVideoUrl}
+          muted
+          type="video/mp4"
+        ></video>
+      );
+      videoContent = (
+        <div className="super-modal-detail">
+          <p>{this.props.data.extract.title}</p>
+          <p>{this.props.data.extract.director}</p>
+          <p>{this.props.data.extract.cast}</p>
+          <p>{this.props.data.extract.summary}</p>
+          <p>{this.props.data.extract.year}</p>
+        </div>
+      );
+      entireModal = (
+        <div className="super-modal">
+          {superModal}
+          {videoContent}
+        </div>
+      );
+      modalBack = <div className="super-modal-background"></div>;
+    }
     return (
       <div
         id={this.props.id}
@@ -94,6 +126,8 @@ class MediaTile extends React.Component {
       >
         {displayContent}
         {buttonContent}
+        {entireModal}
+        {modalBack}
         {/* button content causes weird bulge issue */}
       </div>
     );
