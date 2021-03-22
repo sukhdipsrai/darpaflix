b24c7851dd1ab17e8e412e7609f0dbb623ad1a41
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-03-21 21:54:07 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,12 @@ g1 = Genre.create(name: "Action")
 g2 = Genre.create(name: "Adventure")
 g3 = Genre.create(name: "Comic Book")
 g4 = Genre.create(name: "Thriller")
-g5 = Genre.create(name: "Superhero")
+g5 = Genre.create(name: "Biography")
+g6 = Genre.create(name: "Drama")
+g7 = Genre.create(name: "Comedy")
+g8 = Genre.create(name: "Superhero")
 
-
+## BatmanBegins
 picture = File.open('../data/BatmanBegins/BatmanBeginsImage.webp')
 trailer = File.open('../data/BatmanBegins/BatmanBeginsTrailer.mp4')
 media = trailer
@@ -35,3 +38,30 @@ m1.picture.attach(io: picture, filename: 'BatmanBeginsImage.webp')
 m1.trailer.attach(io: trailer, filename: 'BatmanBeginsTrailer.mp4')
 m1.media = m1.trailer # does nothing, not holding actual media, for future use
 
+## FreeStateOfJones
+picture = File.open('../data/FreeStateOfJones/FreeStateOfJonesImage.webp')
+trailer = File.open('../data/FreeStateOfJones/FreeStateOfJonesTrailer.mp4')
+media = trailer
+m2 = Movie.create(title: "Free State of Jones", cast:"Matthew McConaughey, Gugu Mbatha-Raw, Mahershala Ali", director:"Gary Ross",summary:"A disillusioned Confederate army deserter returns to Mississippi and leads a militia of fellow deserters and women in an uprising against the corrupt local Confederate government.", year: 2016, duration: 2.20)
+
+m2.movies_genres.create(genre: g1)
+m2.movies_genres.create(genre: g5)
+m2.movies_genres.create(genre: g6)
+
+m2.picture.attach(io: picture, filename: 'FreeStateOfJonesImage.webp')
+m2.trailer.attach(io: trailer, filename: 'FreeStateOfJonesTrailer.mp4')
+m2.media = m1.trailer # does nothing, not holding actual media, for future use
+
+## Mank
+picture = File.open('../data/Mank/MankImage.webp')
+trailer = File.open('../data/Mank/MankTrailer.mp4')
+media = trailer
+m2 = Movie.create(title: "Mank", cast:"Gary Oldman, Amanda Seyfried, Lily Collins", director:"David Fincher",summary:"1930's Hollywood is reevaluated through the eyes of scathing social critic and alcoholic screenwriter Herman J. Mankiewicz as he races to finish the screenplay of Citizen Kane (1941).", year: 2020, duration: 2.11)
+
+m2.movies_genres.create(genre: g5)
+m2.movies_genres.create(genre: g7)
+m2.movies_genres.create(genre: g6)
+
+m2.picture.attach(io: picture, filename: 'MankImage.webp')
+m2.trailer.attach(io: trailer, filename: 'MankTrailer.mp4')
+m2.media = m1.trailer # does nothing, not holding actual media, for future use
